9273e01e72d608201af6734c9aa45f3663b6cee1
Textic <danielbienchido@gmail.com>
2025-06-13 20:53:53 -0400

    avance en el login y su logica

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
-	{ path: '', component: HomeComponent }
+	{ path: '', redirectTo: '/home', pathMatch: 'full' },
+	{ path: 'home', component: HomeComponent },
+	{ path: 'profile', component: ProfileComponent },
+	{ path: 'login', component: LoginComponent },
 ];
